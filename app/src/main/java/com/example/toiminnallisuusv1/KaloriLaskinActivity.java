af0de4b1570670b0cc61b12877bc8feb28505d29
t8leta00 <t8leta@students.oamk.fi>
2020-04-09 08:27:36 +0300

    'kalorilaskin'

--- app/src/main/java/com/example/toiminnallisuusv1/KaloriLaskinActivity.java
+++ app/src/main/java/com/example/toiminnallisuusv1/KaloriLaskinActivity.java
@@ -5,11 +5,19 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class KaloriLaskinActivity extends AppCompatActivity implements View.OnClickListener {
+public class KaloriLaskinActivity extends AppCompatActivity {
+
+    Button add, clear;
+    TextView tulos;
+    EditText kcalEdit, amountEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,12 @@ public class KaloriLaskinActivity extends AppCompatActivity implements View.OnCl
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        add = findViewById(R.id.addButton);
+        clear = findViewById(R.id.clearButton);
+        tulos = findViewById(R.id.tulosText);
+        kcalEdit = findViewById(R.id.kcalEditText);
+        amountEdit = findViewById(R.id.amountEditText);
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -47,20 +61,28 @@ public class KaloriLaskinActivity extends AppCompatActivity implements View.OnCl
         startActivity(takaisinIntent);
     }
 
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.laskeButton) {
-            View b = findViewById(R.id.clearButton);
-            View c = findViewById(R.id.addButton);
-            b.setVisibility(View.VISIBLE);
-            c.setVisibility(View.VISIBLE);
-        }
+    public void laskeClick(View view) {
+        String message = amountEdit.getText().toString();
+        int eatenAmount = Integer.parseInt(message);
 
-        else if (v.getId() == R.id.clearButton) {
-            View b = findViewById(R.id.clearButton);
-            View c = findViewById(R.id.addButton);
-            b.setVisibility(View.INVISIBLE);
-            c.setVisibility(View.INVISIBLE);
-        }
+        String message2 = kcalEdit.getText().toString();
+        int kcalAmount = Integer.parseInt(message2);
+        int laskuTulos = kcalAmount * eatenAmount / 100;
+        String tulosString = String.valueOf(laskuTulos);
+
+        add.setVisibility(View.VISIBLE);
+        clear.setVisibility(View.VISIBLE);
+        tulos.setText(tulosString);
+    }
+
+    public void clearClick(View view) {
+        add.setVisibility(View.INVISIBLE);
+        clear.setVisibility(View.INVISIBLE);
+        tulos.setText("");
+        kcalEdit.setText("");
+        amountEdit.setText("");
+    }
+
+    public void addClick(View view) {
     }
 }
