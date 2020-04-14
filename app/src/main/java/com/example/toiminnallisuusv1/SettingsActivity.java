7a78ff88dac0ea7cd58020122a3f487e74c31efc
t8leta00 <t8leta@students.oamk.fi>
2020-04-14 21:00:01 +0300

    settings stuff

--- app/src/main/java/com/example/toiminnallisuusv1/SettingsActivity.java
+++ app/src/main/java/com/example/toiminnallisuusv1/SettingsActivity.java
@@ -14,7 +14,7 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_user2);
+        setContentView(R.layout.activity_settings);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
